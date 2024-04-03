0ea8cf6e42452cd8cb9e9743f29999785397606d
Hyejeong Son <154120089+sonhj0125@users.noreply.github.com>
2024-04-03 17:24:43 +0900

    24.04.03

--- JavaScriptStudy/src/main/webapp/09_javascriptStandardObject/01_Array_class/js/04.js
+++ JavaScriptStudy/src/main/webapp/09_javascriptStandardObject/01_Array_class/js/04.js
@@ -60,7 +60,7 @@ window.onload = function() {
             이렇게 undefined 가 나오지 않도록 하기 위해서 우리는 노드에 id 값을 주어서 처리하도록 한다. 
       */
 
-            
+
         // 단가가 얼마인지 알아보기
         // alert(e.target.parentNode.innerText);   
         // "1,000" "2,000" => 문자열 출력, 콤마를 빼야함
@@ -71,8 +71,8 @@ window.onload = function() {
         let price = e.target.parentNode.innerText;  // "1,000"
         price = Number(price.split(",").join(""));   // ["1","000"] => "1000" => 1000 
         
-        alert(price);
-        alert(typeof price);
+        // alert(price);
+        // alert(typeof price);
 
         
 
