0bb805ca85f233b48de1a33e25503f2b7c7f4dcd
Hyejeong Son <154120089+sonhj0125@users.noreply.github.com>
2024-04-03 16:18:26 +0900

    24.04.03

--- JavaScriptStudy/src/main/webapp/09_javascriptStandardObject/01_Array_class/js/03.js
+++ JavaScriptStudy/src/main/webapp/09_javascriptStandardObject/01_Array_class/js/03.js
@@ -49,7 +49,258 @@ window.onload = function() {
     //////////////////////////////////////////////////////////////////
 
 
+    // ==== td 태그에 나열된 점수를 가져와서 총점 및 평균 구하기 시작 ==== //
+    const td_list = document.querySelectorAll("table#tbl > tbody > tr > td:last-child");                                
+    console.log(td_list);
+    // NodeList(5) [td, td, td, td, td]
+
+    const point_arr = Array.from(td_list, item => item.innerText);
+    console.log(point_arr); // (5) ['90', '100', '50', '70', '80']
+
+    let sum = 0;
+    for(let point of point_arr) {
+        sum += Number(point);
+    }
+
+    console.log("sum => ", sum);
+    // sum =>  390
+
+    console.log("sum/point_arr.length => ", sum/point_arr.length);
+    // sum/point_arr.length =>  78
+
+    document.querySelector("table#tbl > tfoot > tr:first-child > td:last-child").innerHTML = sum; 
     
+    document.querySelector("table#tbl > tfoot > tr:last-child > td:last-child").innerHTML = sum/point_arr.length;  
+    // ==== td 태그에 나열된 점수를 가져와서 총점 및 평균 구하기 끝 ==== //
+
+
+    //////////////////////////////////////////////////////////////////
+
+
+    const arr_member = [{userid:"leess", passwd:"qwer1234$", name:"이순신", age:25},
+                       {userid:"eomjh", passwd:"qwer1234$", name:"엄정화", age:47},
+                       {userid:"seokj", passwd:"qwer1234$", name:"서강준", age:23},
+                       {userid:"sunsin", passwd:"qwer1234$", name:"이순신", age:38},
+                       {userid:"youks", passwd:"qwer1234$", name:"유관순", age:24}];
+    
+    const arr_leess = [];
+   
+    for(let item of arr_member) {
+        if(item.name.trim() == '이순신') {
+           arr_leess.push(item);
+        }
+    }// end of for-----------------                    
+     
+    let html = `<table>
+                    <thead>
+                       <tr>
+                         <th>아이디</th>
+                         <th>암호</th>
+                         <th>성명</th>
+                         <th>나이</th>
+                       </tr>
+                    </thead>
+                    <tbody>`;
+                    
+    if(arr_leess.length > 0) {
+       
+       for(let item of arr_leess) {
+          html += `<tr>
+                      <td>${item.userid}</td>
+                      <td>${item.passwd}</td>
+                      <td>${item.name}</td>
+                      <td>${item.age}</td>
+                    </tr>`;    
+       }// end of for----------------
+    }
+    
+    else {
+       html += `<tr>
+                   <td colspan="4">데이터가 존재하지 않습니다</td>
+                 </tr>`;
+    }
+     
+     html += `  </tbody>
+              </table>`;
+              
+     document.querySelector("div#quiz").innerHTML = html;         
+     
+     //////////////////////////////////////////////////////////////////////////////// 
+
+
+    // ---- **** 배열명.find() **** ----
+    // ES6에서 새로 도입된 메소드로서 판별함수의 조건에 만족하는 배열요소가 있으면 첫번째 배열요소를 리턴 시켜주고 끝내고, 
+    // 판별함수의 조건에 만족하는 배열요소가 없으면 undefinded
+        
+        let member = arr_member.find( function(item, index, array) {
+        // item 은 필수
+        // index, array 은 생략가능하다.
+            if(item.name.trim() == "엄정화") {
+                return item;
+            }                   
+        });
+        
+    console.log(member);                               
+    // {userid: 'eomjh', passwd: 'qwer1234$', name: '엄정화', age: 47}
+
+        
+    member = arr_member.find(item => item.name.trim() == "엄정화");
+    console.log(member);
+    // {userid: 'eomjh', passwd: 'qwer1234$', name: '엄정화', age: 47} 
+
+    member = arr_member.find(item => item.name.trim() == "이순신");
+    console.log(member);
+    // {userid: 'leess', passwd: 'qwer1234$', name: '이순신', age: 25} 
+
+    // === !!!! 암기  배열명.find() 은 고유한 값(primary key)으로 검색할 때 사용한다. !!! === // 
+    member = arr_member.find(item => item.userid == "sunsin");
+    console.log(member);
+    // {userid: 'sunsin', passwd: 'qwer1234$', name: '이순신', age: 38} 
+
+    member = arr_member.find(item => item.userid == "sonhyejeong");
+    console.log(member);
+    // undefined
+
+
+    //////////////////////////////////////////////////////////////
+
+
+    // ---- **** 배열명.filter() **** ---- find는 1개, filter는 여러개
+    // 판별함수의 조건에 만족하는 배열요소가 있으면 모든 배열요소를 담은 배열로 리턴 시켜주고,
+    // 판별함수의 조건에 만족하는 배열요소가 없으면 빈배열 [] 을 리턴시켜준다.   
+
+    let member_arr = arr_member.filter(item => item.name.trim() == "이순신");  
+    console.log(member_arr);
+    // (2) [{…}, {…}]
+
+    if(member_arr.length > 0) {
+
+        for(let mbr of member_arr) {
+
+            for(let property_name in mbr) {
+                console.log(`${mbr[property_name]}`); 
+            }
+            console.log(`--------------------`);
+
+        } // end of for-------------------------
+    } // end of if(member_arr.length > 0)
+
+    /*
+        leess
+        qwer1234$
+        이순신
+        25
+        --------------------
+        sunsin
+        qwer1234$
+        이순신
+        38
+        --------------------
+    */
+
+    member_arr = arr_member.filter(item => item.name.trim() == "손혜정"); 
+    console.log(member_arr);
+    // [] 
+
+
+    //////////////////////////////////////////////////////////////
+
+    // ---- **** 배열명.findIndex() **** ----
+    // ES6에서 새로 도입된 메소드로서 판별함수의 조건에 만족하는 배열요소가 있으면 첫번째 배열요소의 인덱스번호만 리턴 시켜주고 끝내고, 
+    // 판별함수의 조건에 만족하는 배열요소가 없으면 -1 이 나온다.
+
+    // ==> 배열 arr_member 에서 나이가 20대인 회원의 배열인덱스번호를 출력시켜보자. <== // 
+    //  20    ==> 2     21 ==> 2         27 ==> 2        29 ==> 2
+    //  20/10 ==> 2     21/10 ==> 2.1    27/10 ==> 2.7   29/10 ==> 2.9
+    //  Math.floor(2)   ==> 2   
+    //  Math.floor(2.1) ==> 2  
+    //  Math.floor(2.7) ==> 2  
+    //  Math.floor(2.9) ==> 2
+    //  parseInt("2.9") ==> 2
+
+    let idx = arr_member.findIndex(item => Math.floor(item.age/10) == 2); 
+    console.log(idx);
+    // 0
+
+    // === !!!! 암기  배열명.findIndex() 은 고유한 값(primary key)으로 검색할 때 사용한다. !!! === // 
+    idx = arr_member.findIndex(item => item.userid == "seokj"); 
+    console.log(idx);
+    // 2
+
+    console.log(arr_member[idx]);
+    // {userid: 'seokj', passwd: 'qwer1234$', name: '서강준', age: 23}
+
+    if(idx != -1) {
+    for(let property_name in arr_member[idx]) {
+    console.log(arr_member[idx][property_name]);
+    }
+    }
+    /*
+    seokj
+    qwer1234$
+    서강준
+    23
+    */
+
+    idx = arr_member.findIndex(item => item.userid == "munsaehan"); 
+    console.log(idx);
+    // -1
+
+    if(idx == -1) {
+    console.log("userid 가 munsaehan 인 회원은 없습니다.");
+    }
+    else {
+    console.log(arr_member[idx]);
+    }
+    // userid 가 munsaehan 인 회원은 없습니다.
+
+
+    // ---- **** 배열명.some() **** ----
+    // 배열명에서 판별함수의 조건에 만족하는 배열요소가 하나라도 있으면 true 를 리턴 시켜주고 조건검사를 중지한다.
+    // 배열명에서 판별함수의 조건에 만족하는 배열요소가 하나라도 없으면 false 를 리턴 시켜준다.
+
+    arr_member.push({userid:"teen", 
+    passwd:"qwer1234$", 
+    name:"나어려", 
+    age:12});    
+
+    let bool = arr_member.some(item => item.age < 20);
+
+    console.log(bool);
+    // true
+
+    if( arr_member.some(item => item.age < 20) ) {
+    console.log("회원중에 20대 미만인 회원이 존재합니다.");
+    }
+    else {
+    console.log("모든 회원들의 나이는 20세 이상입니다.");
+    }
+    // 회원중에 20대 미만인 회원이 존재합니다.
+
+
+
+    // ---- **** 배열명.every() **** ----
+    // 배열명에서 판별함수의 조건에 만족하는 배열요소가 하나라도 없으면 false 를 리턴 시켜주고 조건검사를 중지한다.
+    // 배열명에서 판별함수의 조건에 만족하는 배열요소가 모두 존재해야만 true 를 리턴 시켜준다.
+
+    bool = arr_member.every(item => item.age >= 10);
+    console.log(bool); 
+    // true
+
+    bool = arr_member.every(item => item.age >= 30);
+    console.log(bool);
+    // false
+
+    if( arr_member.every(item => item.age >= 10) ) {
+    console.log("모든 회원들의 나이는 10세 이상입니다.");
+    }
+    else {
+    console.log("회원중에 나이가 10세 미만인 회원이 존재합니다.");
+    }
+    // 모든 회원들의 나이는 10세 이상입니다.
+
+
+
 
 
 }; // end of window.onload = function() {}
